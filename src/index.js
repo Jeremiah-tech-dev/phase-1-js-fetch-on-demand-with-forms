ca7710e999f2f04e76313553159dd7f69b02db3a
Jeremiah-tech-dev <jeremiah.koske@student.moringaschool.com>
2025-09-19 15:45:36 +0300

    Did the form phase 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener(`submit`, (event) => {
+    event.preventDefault();
+    const input = document.querySelector(`input#searchByID`);
+    console.log(input.value);
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const movieTitle = document.querySelector('section#movieDetails h4');
+        const movieSummary = document.querySelector('section#movieDetails p');
+        movieTitle.textContent = data.title;
+        movieSummary.textContent = data.summary;
+        console.log(data);
+      })
+      .catch((error) => console.error(error));
 
-document.addEventListener('DOMContentLoaded', init);
+    });
+
+  document.addEventListener('DOMContentLoaded', init);
+  }
